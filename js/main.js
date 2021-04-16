8c3832f5ed9432e3c17f4da3b23ea30ffe018615
xLeeVeNx <berdyev.damil1@mail.ru>
2021-04-16 15:49:18 +0600

    fixed

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const render = () => {
   todoList.textContent = '';
   todoCompleted.textContent = '';
 
-  todoData = JSON.parse(localStorage.todoData);
+  todoData = JSON.parse(localStorage.todoData) || [];
 
   todoData.forEach((item) => {
     const li = document.createElement('li');
